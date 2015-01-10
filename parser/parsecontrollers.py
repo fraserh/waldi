36494a68c69cfdb6e18651f59f45b3b7335ecdd7
Fraser Hemphill <z3417550@unsw.edu.au>
2015-01-11 01:29:20 +1100

    Semi-generalised coles. Edge cases are causing a few bugs though.

--- parser/parsecontrollers.py
+++ parser/parsecontrollers.py
@@ -4,11 +4,17 @@
 Collection of helper functions to help parse data
 """
 
-def getInnerHTML(node):
+def getInnerHTML(node, foo, bar):
 	"""Get InnerHTML
     Return the inside of a node (i.e. remove tags)
     """
 	return node.decode_contents(formatter="html")
 
-def prettifyString(text):
+def prettifyString(text, foo, bar):
 	return text.strip().lower()
+
+def splitSplice(node, splitString, section):
+	"""Get InnerHTML
+    Splits a string at point splitString, and returns 'section' item of the resulting list
+    """
+	return node.split(splitString)[section]
--- parser/scrap.py
+++ parser/scrap.py
@@ -1,11 +1,11 @@
+"""
+Generalised Web Scraper
+"""
+
 from bs4 import BeautifulSoup
 import parsecontrollers
 
 soup = BeautifulSoup(open("fruit.html"));
-
-"""
-Generalised Web Scraper
-"""
 
 class pageParser(object):
   """New Page Parser """
@@ -13,43 +13,29 @@ class pageParser(object):
   def __init__(self, html_doc):
   	self.html_doc = html_doc
 
-	self.COLES_PARAMS = [{
-	  	"element":"div",
-	  	"classname": "detail",
-	  	"extra_classname": "product-url",
-	  	"extra_element": "a",
-	  	"decode": 1,
-	  	"prettify": 1
-	  },
-	  {
-	  	"element":"div",
-	  	"classname": "detail"
-	  }
-	  ]
+	self.COLES_PARAMS = {
+	  	"elements_to_search":{
+	  						"tag":["div","a"],
+	  						"class": ["detail", "product-url"]
+	  						},
+	  	"extract_data": [parsecontrollers.getInnerHTML, parsecontrollers.prettifyString, parsecontrollers.splitSplice]
+	}
 
   def get_container(self):
     """Get Container Class Names.
     Get the relevant class names of the containers to be searched.
     """
-    results = self.html_doc.find_all(self.COLES_PARAM[0]["element"],self.COLES_PARAMS[0]["classname"])
-    if ()
-    print results
 
-  def get_containers():
-    """Get Container Class Names.
-    Get the relevant class names of the containers to be searched.
-    """
-    x = ""
+    results = list()
+    results.append(self.html_doc)
 
-for product in soup.find_all("div","wrapper"):
-  title_container = product.find_all("div","detail")[0]
-  title = title_container.find_all("a","product-url")[0].decode_contents(formatter="html").strip().split("&nbsp")[0].lower()
-  price_container =  product.find_all("div","price")
-  price = ""
-  for price_object in price_container:
-   	price = price_object.decode_contents(formatter="html").split("</small>")[1]
-  #print("%s,%s" %(title,price))
+    # TODO: Use get requests on ALL dictionary calls (fail safe) 
+    for searchtag, searchclass in zip(self.COLES_PARAMS["elements_to_search"]["tag"],self.COLES_PARAMS["elements_to_search"]["class"]):
+      results[:] = [match[0].find_all(searchtag,searchclass) for match in results]
+    for extracter in self.COLES_PARAMS["extract_data"]:
+      results[:] = [extracter(entry, "&nbsp", 0) for entry in results]
 
+# Testing purposes.
 foo = pageParser(soup).get_container()
 
 
