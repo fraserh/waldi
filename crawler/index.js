0207037bd788ce932eb29b87a40b9207d5e6d493
matthewpalmer <matt@matthewpalmer.net>
2015-01-12 21:00:16 +1100

    Added timeout to crawler and removed old sample runs

--- crawler/index.js
+++ crawler/index.js
@@ -1,7 +1,9 @@
-var url = 'http://shop.coles.com.au/online/national/pantry/pantry#pageNumber=1000&currentPageSize=20';
+var url = 'http://shop.coles.com.au/online/national/pantry/pantry#pageNumber=5&currentPageSize=20';
 var page = require('webpage').create();
 
 page.open(url, function () {
+  setInterval(function() {
     console.log(page.content); //page source
     phantom.exit();
+  }, 5000);
 });
