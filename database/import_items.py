9c50afdf68d11191b67ba662b09501eafeadd6c2
matthewpalmer <matt@matthewpalmer.net>
2015-03-21 18:09:58 +1100

    Added more csv files for data importing

--- database/import_items.py
+++ database/import_items.py
@@ -56,6 +56,7 @@ def main():
   filename = sys.argv[1]
   with open(filename, 'rb') as csvfile:
     for row in csv.reader(csvfile, skipinitialspace=True):
+      print row
       print line_to_redis(row)
 
 if __name__ == '__main__':
